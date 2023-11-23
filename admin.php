99479ae212f27aaa84311331da85047b4ee7525b
Eduardo9494 <1433341@senati.pe>
2023-11-23 09:32:25 -0500

    hacemos un html

---
+++ admin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Administración - Control de Asistencia</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Control de Asistencia</a>
+</nav>
+
+
+
+
+
+</body>
+</html>
