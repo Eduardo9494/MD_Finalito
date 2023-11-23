fcad0543899245ee31ae2e791da2f91b0b0d0516
Eduardo9494 <1433341@senati.pe>
2023-11-23 10:07:08 -0500

    creamos la conexion a la base de datos

---
+++ procesar_agregar_profesor.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado de Agregar Profesor</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <?php
+        // Conexión a la base de datos
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "asistencia";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Error de conexión a la base de datos: " . $conn->connect_error);
+        }
+
+        if (isset($_POST['nombre']) && isset($_POST['dni'])) {
+            $nombre = $_POST['nombre'];
+            $dni = $_POST['dni'];
+
+            $sql = "INSERT INTO asistencia (nombre, dni) VALUES ('$nombre', '$dni')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo '<div class="alert alert-success" role="alert">Profesor agregado con éxito.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error al agregar al profesor: ' . $conn->error . '</div>';
+            }
+        }
+
+        $conn->close();
+        ?>
+
+        <a href="agregar_profesor.php" class="btn btn-primary">Agregar más profesor</a>
+        <a href="panel_admin.php" class="btn btn-secondary">Volver al panel de admin</a>
+    </div>
+    
+</body>
+</html>
