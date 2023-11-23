b0c628401679093339111437325f0cfa3bd98b39
Eduardo9494 <1433341@senati.pe>
2023-11-23 08:56:11 -0500

    cambiamos contraseña

--- procesar_login_admin.php
+++ procesar_login_admin.php
@@ -3,7 +3,7 @@ session_start();
 
 // Verificar las credenciales de administrador (esto es solo un ejemplo, asegúrate de implementar una autenticación segura)
 $usuario_correcto = "admin";
-$contrasena_correcta = "contrasena123";
+$contrasena_correcta = "admin";
 
 $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
