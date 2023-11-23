ac9f7ddb62d8668a9d9e93fc9d8fafc856da4bee
Eduardo9494 <1433341@senati.pe>
2023-11-23 09:40:58 -0500

    creamos u n html en el arcivo agregar_profesor

---
+++ agregar_profesor.php
@@ -0,0 +1,19 @@
+<!-- agregar_profesor.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agregar Profesor</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="admin.php">Panel del Administrador</a>
+</nav>
+
+
+
+</body>
+</html>
