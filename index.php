6059078a1efb7c1276e95faa2a74f259215dc6ba
Eduardo9494 <1433341@senati.pe>
2023-11-23 10:02:30 -0500

    creamos un html en index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="https://th.bing.com/th/id/R.9410aeb94b89a3d2642c3397aee02704?rik=11nccvc0wQFIFA&pid=ImgRaw&r=0" type="image/png">
+    <title>Control de Asistencia</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+
+
+</body>
+</html>
