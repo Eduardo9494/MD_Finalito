806e8db2f5f2d0cf694e23193f7dd932ab3f996e
Eduardo9494 <1433341@senati.pe>
2023-11-23 09:58:47 -0500

    codificamos en eliminar_profesores.php

---
+++ eliminar_profesor.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost"; // Cambia esto si tu servidor de base de datos tiene una ubicación diferente
+$username = "root"; // Nombre de usuario de la base de datos (por defecto, en XAMPP es "root")
+$password = ""; // Contraseña de la base de datos (deja en blanco si no tienes una configurada)
+$dbname = "asistencia"; // Nombre de tu base de datos
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+?>
