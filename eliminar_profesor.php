7b29da1ef7d9fe8791c53aef4b5700bea9320e9d
Eduardo9494 <1433341@senati.pe>
2023-11-23 09:59:29 -0500

    verificamos la conexion

--- eliminar_profesor.php
+++ eliminar_profesor.php
@@ -6,5 +6,10 @@ $dbname = "asistencia"; // Nombre de tu base de datos
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+if ($_SERVER["REQUEST_METHOD"] === "GET" && isset($_GET['dni'])) {
+    $dni = $_GET['dni'];
 ?>
